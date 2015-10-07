78480c79fc497e6937046bd15bbdfb3212795d94
goblin9700 <alexgrist@yandex.ua>
2015-10-07 18:01:13 +0300

    new class

--- src/portalLoc/Main.java
+++ src/portalLoc/Main.java
@@ -1,18 +1,20 @@
 package portalLoc;
 
 import java.util.logging.Logger;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.PortalCreateEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin{
 	
 	Logger log = getLogger();
 	
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
+		getServer().getPluginManager().registerEvents(new PortalCreater(), this);
 		log.info("PortalLoc on enabled.");
 		log.info("Создание порталов отключено.");
 	}
@@ -22,12 +24,15 @@ public class Main extends JavaPlugin implements Listener {
 		log.info("PortalLoc has disabled.");
 	}
 	
-	@EventHandler
-	public void portalLoc(PortalCreateEvent event) {
-		event.getPlayer().sendMessage("Create portal has disable.");
-		event.setCancelled(true);
-		
-		
+	private class PortalCreater implements Listener {
+		Player player;
+		@EventHandler
+		public void portalLock(PortalCreateEvent event) {
+			if (player == null) {
+				player.sendMessage("Create portal has disable.");
+				event.setCancelled(true);
+			}
+		}
 	}
 
 }
